f8cc61f6006a217dfc410c3b5a5ef50919c3f04d
philipnathan <philipnathan168@gmail.com>
2024-09-24 12:05:15 +0800

    add blacklist token feature

--- backend/scm/settings.py
+++ backend/scm/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "apps.user",
 ]
 
@@ -145,4 +146,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(weeks=1),
     "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
