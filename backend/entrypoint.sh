f7e0cb68cb04eb7de76bc3b353b0ea04f69abe4f
philipnathan <philipnathan168@gmail.com>
2024-10-06 13:13:18 +0800

    add: makemigrations command

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,10 +4,11 @@ echo "Waiting for PostgreSQL..."
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+  sleep
 done
 
 echo "PostgreSQL started"
 
+python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
